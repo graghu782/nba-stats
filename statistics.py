ff813cf6998995e43ab6a0553bb8ce513e1686fe
williamwu <wwu062@student.fuhsd.org>
2019-05-21 13:29:21 -0700

    changed EXCEL to start from 1980

--- statistics.py
+++ statistics.py
@@ -1,6 +1,6 @@
 import pandas as pd
+season_data = pd.read_csv("Seasons_Stats.csv")
 import numpy as np
-season_data = pd.read_csv("/Users/graghu/Desktop/Coding Workspaces/training pages/nba-stats/Seasons_Stats.csv")
 print(season_data[0:5])
 
 df = pd.DataFrame(season_data)
@@ -9,4 +9,3 @@ category_medians = df.median()
 # dictionary of standard deviations
 category_sds = df.std()
 
-print(df.max())
